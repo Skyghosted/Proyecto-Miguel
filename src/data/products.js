5455090d5b1ad982193bf22622e526486a11f4de
Roberto <robertosanchez0311@gmail.com>
2025-12-17 00:54:24 +0100

    No se puede mas

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,10 @@
 const PRODUCTS = [
-  { id: 'p1', name: 'Camiseta Essential', price: 18.99, stock: 24, image: '/images/Camiseta.png', sizes: ['S','M','L'] },
-  { id: 'p2', name: 'Sudadera Oversize', price: 44.99, stock: 12, image: '/images/Sudadera.png', sizes: ['M','L','XL'] },
-  { id: 'p3', name: 'Pantalón Straight', price: 52.00, stock: 8, image: '/images/Pantalon.png', sizes: ['S','M','L'] },
-  { id: 'p4', name: 'Chaqueta Acolchada', price: 79.50, stock: 5, image: '/images/Acolchada.png', sizes: ['M','L','XL'] },
-  { id: 'p7', name: 'Gorra Sport', price: 16.50, stock: 30, image: '/images/Gorra.png', sizes: [] },
-  { id: 'p8', name: 'Calcetines Pack 3', price: 11.99, stock: 40, image: '/images/Calcetines.png', sizes: [] },
+  { id: 'p1', name: 'Camiseta Essential', price: 18.99, stock: 24, image: '/Camiseta.png', sizes: ['S','M','L'] },
+  { id: 'p2', name: 'Sudadera Oversize', price: 44.99, stock: 12, image: '/Sudadera.png', sizes: ['M','L','XL'] },
+  { id: 'p3', name: 'Pantalón Straight', price: 52.00, stock: 8, image: '/Pantalon.png', sizes: ['S','M','L'] },
+  { id: 'p4', name: 'Chaqueta Acolchada', price: 79.50, stock: 5, image: '/Acolchada.png', sizes: ['M','L','XL'] },
+  { id: 'p7', name: 'Gorra Sport', price: 16.50, stock: 30, image: '/Gorra.png', sizes: [] },
+  { id: 'p8', name: 'Calcetines Pack 3', price: 11.99, stock: 40, image: '/Calcetines.png', sizes: [] },
 ]
 
 export default PRODUCTS
